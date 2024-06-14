6fca21a29f852307d1d284c14f1e6dea6684cae0
Zaluskii Vitalii <vzaluskiy23@gmail.com>
2024-06-14 17:23:45 +0300

    2

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const submitBtn = document.querySelector('.form__btn');
+
+class newWorkout {
+  constructor(distance, duration, cadence, elevation) {
+    this.distance = distance;
+    this.duration = duration;
+    this.cadence = cadence;
+    this.elevation = elevation;
+  }
+}
+
+class Cycling extends newWorkout {
+  constructor() {
+    super(distance, duration, cadence, elevation);
+  }
+}
+
+class Running extends newWorkout {
+  constructor() {
+    super(distance, duration, cadence, elevation);
+  }
+}
+
+const run1 = new Running(23, 60, 2, 234);
+console.log(run1);
 
 class App {
   constructor() {
     this.getCurrentLocation;
     this.loadMap;
+    this.newMarker;
   }
 
   getCurrentLocation() {
@@ -31,16 +57,41 @@ class App {
   }
   loadMap(latitude, longitude) {
     const map = L.map('map').setView([latitude, longitude], 13);
+    this.newMarker(map);
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    // }).addTo(map);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    const googleStreets = L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+      {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }
+    );
+
+    googleStreets.addTo(map);
 
     L.marker([latitude, longitude])
       .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .bindPopup(
+        L.popup({
+          autoClose: false,
+        })
+      )
       .openPopup();
+  }
+  newMarker(map) {
+    map.on('click', e => {
+      const { lat, lng } = e.latlng;
+      L.marker([lat, lng]).addTo(map).bindPopup().openPopup();
+      form.classList.remove('hidden');
+    });
+
+    // submitBtn.addEventListener('submit', e => {
+    //   L.marker([latitude, longitude]).popup('Hahahahah');
+    // });
   }
 }
 
